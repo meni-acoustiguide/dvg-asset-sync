41c1bdb6bbd68222f7b54aa6a57a395f33baa0ab
Matt Kane <ascorbic@gmail.com>
2018-02-28 11:56:13 +0000

    Recompile

--- dist/index.js
+++ dist/index.js
@@ -4,26 +4,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const asyncro_1 = require("asyncro");
 const fs = require("fs-extra");
 const mp = require("multi-progress");
-const yargs = require("yargs");
 const DataLoader_1 = require("./lib/DataLoader");
-const argv = yargs
-    .options({
-    configFile: {
-        alias: "c",
-        demandOption: true,
-        describe: "Config file (update_profile.json)"
-    },
-    dir: {
-        alias: "d",
-        describe: "Target directory for files",
-        demandOption: true
-    },
-    saveJSON: {
-        alias: "j",
-        describe: "Filename to save the new JSON if successful"
-    }
-})
-    .help().argv;
 exports.download = async (configFile, dir, saveJSON) => {
     const dvgConfig = await DataLoader_1.loadConfig(configFile);
     const config = {
@@ -68,5 +49,4 @@ exports.download = async (configFile, dir, saveJSON) => {
     }
     return true;
 };
-exports.download(argv.configFile, argv.dir, argv.saveJSON).then(result => process.exit(result ? 0 : -1));
 //# sourceMappingURL=index.js.map
